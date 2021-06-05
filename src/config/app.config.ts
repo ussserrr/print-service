6839aa08f16e48fffd387158edc8355c797a5de9
ussserrr <andrei4.2008@gmail.com>
2021-06-06 01:40:27 +0300

    Add clarification comment

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,7 +5,7 @@ import { registerAs } from '@nestjs/config';
 
 const config = {
   storagePath: process.env.STORAGE_PATH || path.join(process.cwd(), 'storage'),
-  filesToKeep: 5,
+  filesToKeep: 5,  // number of files per template type
   allowedFileTypes: [{
     extension: 'DOCX',
     mime: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
